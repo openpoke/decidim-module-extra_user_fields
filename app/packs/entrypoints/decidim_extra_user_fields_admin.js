d6e7fd55903d4182d33d758840e08b5fb373f58b
Ivan Vergés <ivan@pokecode.net>
2026-03-04 19:53:20 +0100

    minor fixes

--- app/packs/entrypoints/decidim_extra_user_fields_admin.js
+++ app/packs/entrypoints/decidim_extra_user_fields_admin.js
@@ -1,5 +1,5 @@
 import { Application } from "@hotwired/stimulus"
 import FieldStateController from "src/decidim/extra_user_fields/admin/field_state_controller"
 
-const application = Application.start()
+const application = window.Stimulus || Application.start()
 application.register("field-state", FieldStateController)
